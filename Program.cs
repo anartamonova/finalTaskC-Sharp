cce8cbf8ffb4af2d11684f9a4542d6ce9698a6ce
Anna Artamonova <tw1ssss@yandex.ru>
2023-03-07 22:32:41 +0300

    Added program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите массив данных = ");
+string m = Console.ReadLine();
+string[] array = m.Split(' ');
+string[] newArray = new string[array.Length];
+void PrintArray(string[] arr)
+{   Console.Write("[ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine("]");
+   
+};
+PrintArray(array);
+
+void resultArray(string[] arrayAdded, string[] arrayRes) {
+    int count = 0;
+    for ( int i = 0; i < arrayAdded.Length; i ++) {
+        if (arrayAdded[i].Length < 4) {
+            arrayRes[count] = arrayAdded[i];
+            count ++;
+
+        }
+    }
+}
+resultArray(array, newArray);
+PrintArray(newArray);
